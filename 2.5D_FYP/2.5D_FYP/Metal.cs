21413dc84ff6b0661222536d786ca7c7a6cd6253
William Wong <williamwong89@gmail.com>
2013-02-26 13:37:33 +0000

    test

---
+++ 2.5D_FYP/2.5D_FYP/Metal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace _2._5D_FYP
+{
+    class Metal : FloatingObjects
+    {
+        Random randomGenerator = new Random(DateTime.Now.Millisecond);
+        public List<Entity> materialList = new List<Entity>();
+
+        int materialCount = 25;
+
+        FloatingObjects[] material;
+
+        public Metal() 
+        {
+            material = new FloatingObjects[materialCount];
+            _entityName = "Models//StationMaterial";
+        } 
+
+        public void CreateMaterialList() 
+        {
+            for (int i = 0; i < materialCount; i++)
+            {
+                if (materialList.Count < materialCount)
+                {
+                    material[i] = new FloatingObjects();
+                    material[i].Initialize();
+                    material[i]._entityName = _entityName;
+                    material[i]._pos = new Vector3(randomGenerator.Next(-900, 900), 50, randomGenerator.Next(-900, 900));
+                    materialList.Add(material[i]);
+                }
+
+                Game1.Instance().Children.Add(materialList.ElementAt(i));
+            }
+        }
+    }
+}
---
+++ 2.5D_FYP/2.5D_FYP/Weapon.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace _2._5D_FYP
+{
+    public class Weapon : Entity
+    {
+        int weaponIndex = 0;
+        float lastBulletFired = 0.0f;
+        Bullet bullet;
+        Player player;
+
+        KeyboardState keyState;
+        bool fireWeaponPressed = false;
+
+        public Weapon()
+        {
+        }
+
+        public void Update(int index, GameTime gameTime)
+        {
+            float timeDelta = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            keyState = Keyboard.GetState();
+
+            weaponIndex = index;
+
+            lastBulletFired += timeDelta;
+
+            base.Update(gameTime);
+        }
+
+        public void CheckWeaponFire()
+        {
+            player = Game1.Instance().Player;
+            bullet = new Bullet();
+
+            switch (weaponIndex)
+            {
+                case 0:
+                    {
+                        bullet._entityName = "Models//sphere";
+
+                        if(keyState.IsKeyDown(Keys.Space))
+                        {
+                            if (!fireWeaponPressed)
+                            {
+                                 FireBullet(player._pos, player._look);
+                                 fireWeaponPressed = true;
+                            }
+                        }
+                        else fireWeaponPressed = false;
+                    }
+                    break;
+
+                case 1:
+                    {
+                        bullet._entityName = "Models//sphere";
+
+                        if (keyState.IsKeyDown(Keys.Space) && lastBulletFired >= 0.1f)
+                        {
+                            lastBulletFired = 0.0f;
+
+                            FireBullet(player._pos, player._look);
+                        }
+                    }
+                    break;
+
+                case 2: 
+                    {
+                        bullet._entityName = "Models//Station";
+
+                        if (keyState.IsKeyDown(Keys.Space) && lastBulletFired >= 3.0f)
+                        {
+                            lastBulletFired = 0.0f;
+
+                            FireBullet(player._pos, player._look);
+                        }
+                    }
+                    break;
+
+                case 3:
+                    {
+                        //bullet._entityName = "Models//sphere";
+                    }
+                    break;
+            }
+        }
+
+        public void FireBullet(Vector3 pos, Vector3 look)
+        {
+            bullet.LoadContent();
+            bullet._pos = pos;
+            bullet._look = look;
+            Game1.Instance().Children.Add(bullet);
+        }
+    }
+}
